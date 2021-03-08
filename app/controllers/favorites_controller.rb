bad35c5cde443e8acbfc82e952d5bfbfd9a07f52
abi0116 <kosukeabiru.www@gmail.com>
2021-03-08 11:48:22 +0000

    [Start] 6 非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,17 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    comment = current_user.favorites.new(book_id: @book.id)
     comment.save
-    redirect_back fallback_location: root_path
+    # redirect_back fallback_location: root_path
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    comment = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    comment = current_user.favorites.find_by(book_id: @book.id)
     comment.destroy
-    redirect_back fallback_location: root_path
+    # redirect_back fallback_location: root_path
   end
 
 end
