3ea6d9ae58d34df850785943fd5dd611756965a2
abi0116 <kosukeabiru.www@gmail.com>
2021-03-09 07:24:18 +0000

    [Finish] 6 非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,10 +2,11 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @book_comment = current_user.book_comments.new(book_comment_params)#フォームに渡した@book_comment = BookComment.new に@book_commentを合わせてる
-    @book_comment.book_id = @book.id
-    if @book_comment.save
-      redirect_to book_path(@book.id)
+    @book_comment = BookComment.new
+    book_comment = current_user.book_comments.new(book_comment_params)#フォームに渡した@book_comment = BookComment.new に@book_commentを合わせてる
+    book_comment.book_id = @book.id
+    if book_comment.save
+      #redirect_to book_path(@book.id)
     else
       @booknew = Book.new
       @user = User.find(@book.user_id)
@@ -14,8 +15,10 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
     BookComment.find_by(id: params[:id],book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    #redirect_to book_path(params[:book_id])
   end
 
   private
